9643afabd45b20d9d8fcb659cf2b3d0ae2384cea
Minh Duy <126150563+minhduy0909@users.noreply.github.com>
2023-06-30 10:44:42 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Hello GUYs' });
+});
+
+const URI = process.env.MONGOOSE_URL;
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+  }
+);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Server is running on port', port);
+});
